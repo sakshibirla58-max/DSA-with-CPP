6df4ea26a521289c92ffc95f353023ee45f08b12
sakshibirla58-max <sakshibirla58@gmail.com>
2026-05-29 19:40:49 +0530

    Create array_input_output.cpp

---
+++ Arrays/array_input_output.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main() {
+    int marks[5] = {99, 56, 89, 74, 96}, multiply;
+    //marks[0] = 101;
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
+
+    // int arr[5] = {1,2,3,4,5};
+    // for(int i=0; i<5; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+    return 0;
+}
